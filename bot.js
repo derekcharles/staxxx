ce5d6909fed9a764b3048f33ec70c3a8e2d29218
derek-charles <52976014+derek-charles@users.noreply.github.com>
2024-09-29 17:38:10 -0500

    update_remo

--- bot.js
+++
@@ -1,9 +0,0 @@
-const { Bot } = require("grammy");
-
-const bot = new Bot("7571681882:AAE7_MELOkG1uEHgeU77oe2nmKEsUMKNQMA");
-
-// Listening for messages
-bot.on("message:text", (ctx) => ctx.reply("Received: " + ctx.message.text));
-
-// Starting the bot
-bot.start();
