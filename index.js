92bcb66d00be63279e1158e7f6b78dbcc0c53e8a
derek-charles <52976014+derek-charles@users.noreply.github.com>
2024-09-29 18:11:20 -0500

    bizchat

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ require('@dotenvx/dotenvx').config()
 const bot = new Bot(process.env.ACCESS_KEY);
 
 // Listening for messages
-bot.on("message:text", (ctx) => ctx.reply("Received: " + ctx.message.text));
-
+bot.on("start", async (ctx) => {
+    const payload = ctx.match;
+    if(payload.startsWith("bizChat")){
+        const id = payload.slice(7);
+        await ctx.reply(`Let's manage chat ${id}`);
+    }
+}),
 // Starting the bot
 bot.start();
