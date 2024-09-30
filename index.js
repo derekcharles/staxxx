f1ae50aadec41d5dee5277704592c0a99c0ecd30
derek-charles <52976014+derek-charles@users.noreply.github.com>
2024-09-30 07:12:32 -0500

    test

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const { Bot } = require("grammy");
 require('@dotenvx/dotenvx').config({path: __dirname + '/.env'})
 
 const bot = new Bot(process.env.ACCESS_KEY);
-
-// Listening for messages
-bot.command("start", async (ctx) => {
-    const payload = ctx.match;
-    if(payload.startsWith("bizChat")){
-        const id = payload.slice(7);
-        await ctx.reply(`Let's manage chat ${id}`);
-    }
-}),
+const products = [
+    {
+        name: 'Saucy - Staxx Unlimited Raceplay Custom',
+        price: 20.00,
+        description: 'Someone ordered a custom through Saucy',
+        photoUrl: 'http://vignette2.wikia.nocookie.net/fallout/images/e/e6/Fallout4_Nuka_Cola_Quantum.png'
+    },
+    {
+        name: 'Custom',
+        price: 15.00,
+        description: 'Price Per minute',
+        photoUrl: 'https://vignette2.wikia.nocookie.net/fallout/images/b/b9/Iguana_on_a_stick.png'
+  }
+]
+bot.command("start", (ctx) => ctx.reply("Hello what would you be interested in ?"))
+bot.on("message", (ctx) => ctx.reply("Gotcha!"));
 // Starting the bot
 bot.start();
